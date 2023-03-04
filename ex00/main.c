c5798c711016221448f1b8e9c5014c5a0810ab45
Timothe <petertimpan@gmail.com>
2023-03-04 19:11:27 +0100

    ready to publish

--- ex00/main.c
+++ ex00/main.c
@@ -1,5 +1,23 @@
-int main()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmorel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 18:40:12 by tmorel            #+#    #+#             */
+/*   Updated: 2023/03/04 19:09:40 by tmorel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	print_long(int n);
+void	print_ver(int n);
+void	print_fin(int n);
+void	rush(int x, int y);
+
+int	main(void)
 {
-    rush(5, 3);
-    return (0);
+	rush(5, 3);
+	return (0);
 }
--- ex00/rush01.c
+++ ex00/rush01.c
@@ -1,92 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmorel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 18:59:04 by tmorel            #+#    #+#             */
+/*   Updated: 2023/03/04 19:10:33 by tmorel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
 void	print_long(int n)
 {
-    int	count;
+	int	count;
 
-    count = 1;
-
-    while (count <= n)
-    {
-        if (count == 1)
-        {
-            ft_putchar('/');
-        }
-        else if (count == n)
-        {
-        	ft_putchar(92);
-        }
-        else
-        {
-            ft_putchar('*');
-        }
-        count++;
-    }
+	count = 1;
+	while (count <= n)
+	{
+		if (count == 1)
+		{
+			ft_putchar('/');
+		}
+		else if (count == n)
+		{
+			ft_putchar(92);
+		}
+		else
+		{
+			ft_putchar('*');
+		}
+		count++;
+	}
 }
 
 void	print_ver(int n)
 {
-    int	count;
+	int	count;
 
-    count  = 1;
-    while (count <= n)
-    {
-        if  ((count == 1) || (count == n))
-        {
-            ft_putchar('*');
-        }
-        else
-        {
-            ft_putchar(' ');
-        }
-        count++;
-    }
-    
+	count = 1;
+	while (count <= n)
+	{
+		if ((count == 1) || (count == n))
+		{
+			ft_putchar('*');
+		}
+		else
+		{
+			ft_putchar(' ');
+		}
+		count++;
+	}
 }
 
-void     print_fin(int n)
+void	print_fin(int n)
 {
-    int     count;
+	int	count;
 
-    count = 1;
-
-    while (count <= n)
-    {
-        if (count == 1)
-        {
-            ft_putchar(92);
-        }
-        else if (count == n)
-        {
-        	ft_putchar('/');
-        }
-        else
-        {
-            ft_putchar('*');
-        }
-        count++;
-    }
+	count = 1;
+	while (count <= n)
+	{
+		if (count == 1)
+		{
+			ft_putchar(92);
+		}
+		else if (count == n)
+		{
+			ft_putchar('/');
+		}
+		else
+		{
+			ft_putchar('*');
+		}
+		count++;
+	}
 }
 
-
-void    rush(int x, int y)
+void	rush(int x, int y)
 {
-    int     i;
+	int	i;
 
-    i = 1;
-    while (i <= y)
-    {  
-        if (i == 1) 
-        {
-            print_long(x);
-        }
-        else if (i == y)
-        {
-        	print_fin(x);
-        }
-        else
-        {
-            print_ver(x);
-        }
-        ft_putchar(10);
-        i++;
-    }   
+	i = 1;
+	while (i <= y)
+	{
+		if (i == 1)
+		{
+			print_long(x);
+		}
+		else if (i == y)
+		{
+			print_fin(x);
+		}
+		else
+		{
+			print_ver(x);
+		}
+		ft_putchar(10);
+		i++;
+	}
 }
